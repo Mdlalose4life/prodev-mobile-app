99eab46b00a111abc0e8e41a5dcd22fe2d51ca5d
Mdlalose4life <blessing1059@gmail.com>
2025-07-23 13:12:33 +0200

    Navigation using Stack Navigation Patterns

---
+++ prodev-mobile-app-0x04/app/join.tsx
@@ -0,0 +1,15 @@
+import { Text, View } from "react-native";
+
+export default function Join() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Logi in</Text>
+    </View>
+  );
+}
